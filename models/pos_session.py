31acdeea35a7e2f9ec0d18b32fd7053ce804c669
onlyone-odoo <matiasb@onlyone.com.ar>
2025-05-17 16:01:18 -0300

    fix pos_session.py

--- models/pos_session.py
+++ models/pos_session.py
@@ -2,14 +2,9 @@ from odoo import models
 
 
 class PosSession(models.Model):
-    _inherit = 'pos.session'
+    _inherit = "pos.session"
 
     def _loader_params_product_product(self):
         result = super()._loader_params_product_product()
-        result['search_params']['fields'].append('replenishment_cost')
-        return result
-
-    def _loader_params_product_pricelist(self):
-        result = super()._loader_params_product_pricelist()
-        result['search_params']['fields'].append('base')
+        result["search_params"]["fields"].append("replenishment_cost")
         return result
